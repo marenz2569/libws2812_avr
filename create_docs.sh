6c1a3616a0273bb670883f061555ad806210d981
Markus Schmidl <markus.schmidl@ymail.com>
2016-06-12 13:20:26 +0200

    add shell script to create docs on gh-pages branch

---
+++ create_docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+	echo "Please enter a git commit message"
+	exit
+fi
+
+cd html &&\
+	rm -rf * &&\
+	cd .. &&\
+	make docs &&\
+	cd html &&\
+	git commit -am "$1" &&\
+	git push origin gh-pages
